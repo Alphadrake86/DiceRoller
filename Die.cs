07ade05e119b40f7ef9ea0adb87dbd375e4af4c1
Lunaris <aerik0712@gmail.com>
2020-07-14 13:12:46 -0700

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiceRoller
+{
+    class Die
+    {
+        static Random random;
+
+        public byte sides { get; private set; }
+        public byte value { get; set; }
+        public bool isHeld { get; set; }
+
+        static Die()
+        {
+            random = new Random();
+        }
+
+        public Die(): this(6){}
+
+        public Die(byte sides)
+        {
+            this.sides = sides;
+            Roll();
+        }
+
+        public byte Roll()
+        {
+            if (isHeld)
+            {
+                return value;
+            }
+
+            value = (byte)random.Next(sides + 1);
+            return value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die[] dice = new Die[15];
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+            }
+
+            foreach (Die die in dice)
+            {
+                Console.WriteLine(die.value);
+            }
+        }
+    }
+}
